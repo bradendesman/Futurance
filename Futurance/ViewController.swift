e9af4ca987f23f1a770d079a8e8d5e70ad8c5798
bradendesman <braden.desman@gmail.com>
2022-03-26 12:20:14 -0400

    Initial Commit

---
+++ Futurance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Futurance
+//
+//  Created by Braden Desman on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
