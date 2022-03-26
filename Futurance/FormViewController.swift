a26b62c74874e9ec90ccc467ef7d184c5bac5adf
bradendesman <braden.desman@gmail.com>
2022-03-26 14:19:44 -0400

    Two pages done

--- Futurance/FormViewController.swift
+++ Futurance/FormViewController.swift
@@ -7,6 +7,122 @@
 
 import UIKit
 
-class FormViewController: UIViewController {
+class FormViewController: UIViewController, UITextFieldDelegate {
     
+    var pageNumber: Int = 0
+    @IBOutlet var continueButton: UIButton!
+    
+    //MARK: Page 0 Outlets
+    @IBOutlet var pageOneStack: UIStackView!
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    
+    //MARK: Page 1 Outlets
+    @IBOutlet var pageTwoStack: UIStackView!
+    @IBOutlet var pageTwoChoices: [UIStackView]!
+    
+    
+    
+    var fields: [UIControl]!
+    var pages: [UIStackView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureFields()
+        
+        
+    }
+    
+    
+    //MARK: Page 0 Logic
+    @IBAction func firstNameTextFieldChangd(_ sender: Any) {
+        updateContinueButton()
+    }
+    @IBAction func lastNameTextFieldChanged(_ sender: Any) {
+        updateContinueButton()
+    }
+    @IBAction func ageTextFieldChanged(_ sender: Any) {
+        updateContinueButton()
+    }
+    @IBAction func emailTextFieldChanged(_ sender: Any) {
+        updateContinueButton()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Page 1 Logic
+    @IBAction func workStatusButtonPressed(_ sender: UIButton) {
+        let stack = sender.superview as! UIStackView
+        stack.arrangedSubviews[0].isHidden.toggle()
+    }
+    
+    
+    
+    //MARK: Update Button Logic
+    
+    @IBAction func presentNextQuestions(_ sender: Any) {
+        if pageNumber == (pages.count - 1) {
+            print("end")
+        }
+        else {
+            pages[pageNumber].isHidden = true
+            pageNumber += 1
+            pages[pageNumber].isHidden = false
+        }
+        
+    }
+    
+    func configureFields() {
+        pages = [pageOneStack, pageTwoStack]
+        fields = [firstNameTextField, lastNameTextField,ageTextField,emailTextField]
+        
+        for field in fields {
+            guard let currentField = field as? UITextField else {return}
+            currentField.delegate = self
+            currentField.autocapitalizationType = .words
+        }
+        for page in pages {
+            if page != pageOneStack {
+                page.isHidden = true
+            }
+        }
+        
+        
+        for i in 0...pageTwoChoices.count-1 {
+            pageTwoChoices[i].arrangedSubviews[0].isHidden = true
+        }
+        continueButton.isEnabled = false
+    }
+    func updateContinueButton() {
+        if pageNumber == 0 {
+            guard let firstName = firstNameTextField.text else {return}
+            guard let lastName = lastNameTextField.text else {return}
+            guard let age = ageTextField.text else {return}
+            guard let email = emailTextField.text else {return}
+            guard !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty, !age.isEmpty else {return}
+            
+            guard let ageNumber = Int(age), ageNumber > 0 else {return}
+            let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' "
+            
+            for char in firstName {
+                if !allowedChars.contains(char) {return}
+            }
+            for char in lastName {
+                if !allowedChars.contains(char) {return}
+            }
+            for char in email {
+                if "@.".contains(char) {
+                    continue
+                }
+                if !allowedChars.contains(char) {return}
+            }
+            continueButton.isEnabled = true
+        }
+        
+    }
 }
