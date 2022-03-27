486af8f1beab3959ac13e251944e76f67eda1eb3
Joe Khosbayar <joekhosbayar123@gmail.com>
2022-03-26 22:17:00 -0400

    joe2

--- Futurance/AppDelegate.swift
+++ Futurance/AppDelegate.swift
@@ -10,9 +10,7 @@ import Firebase //adding firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure() //configuring firebase
