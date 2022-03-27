5f4d2e5976ab5ca57bf0b4e058c3d2a01532fa2a
Joe Khosbayar <joekhosbayar123@gmail.com>
2022-03-27 11:20:04 -0400

    graph working

--- Futurance/StatusViewController.swift
+++ Futurance/StatusViewController.swift
@@ -16,34 +16,49 @@ class StatusViewController:
     var user: User!
     var savings: [User.GoalType : Float]!
     var networth:Float = 0.0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(lineChartView)
+        lineChartView.centerInSuperview()
+        lineChartView.width(to: view)
+        lineChartView.heightToWidth(of: view)
+        assets()
+        projectedReturns()
+       // dataPoints()
+        setData()
+        
+    }
+    
     func assets(){
         for (type,value) in savings{
             if (type == User.GoalType.studentLoans){
             networth += -1 * savings[type]!
             }   else{
                 networth += value
+                print(type,value)
             }
         }
     }
     var percent = 0.00
+    var data: [Float] = []
     func projectedReturns(){
         switch user.riskTolerance{
         case .high:
-            percent = 14.00
+            percent = 1.14
         case .medium:
-            percent = 8.00
+            percent = 1.08
         case .low:
-            percent = 4.00
+            percent = 1.04
         default:
-            percent = 4.00
+            percent = 1.04
         }
         var years = 65 - user.age
         var data: [Float] = [networth]
         
     func dataPoints(){
         for i in 1...years {
-            let x = (data[0] * Float((1.00 + percent/100.0)))
-            var point = pow(x, Float(i))
+            let x = data[0]
+            var point = x * Float(percent) * Float(i)
             data.append(point)
             xy.append(ChartDataEntry(x:Double(i), y: Double(point)))
         }
@@ -65,16 +80,5 @@ class StatusViewController:
         let set1 = LineChartDataSet(entries: xy, label: "Projected Growth")
         let data = LineChartData(dataSet: set1)
         lineChartView.data = data
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(lineChartView)
-        lineChartView.centerInSuperview()
-        lineChartView.width(to: view)
-        lineChartView.heightToWidth(of: view)
-        assets()
-        projectedReturns()
-       // dataPoints()
-        setData()
     }
 }
